730668ae32a8f4145f869078cf7da1c2d4f8b07e
saantigh <santi.gonzalezhincapie@gmail.com>
2024-10-13 17:55:19 -0500

    Se agrega la clase padre

---
+++ Modelo/CLASE_productos_de_control.py
@@ -0,0 +1,8 @@
+class ProductoControl:
+    def __init__(self, nombre_producto, precio_producto, registro_ica, frecuencia_aplicacion):
+        self.nombre_producto = nombre_producto
+        self.precio_producto = precio_producto
+        self.registro_ica = registro_ica
+        self.frecuencia_aplicacion = frecuencia_aplicacion
+
+
