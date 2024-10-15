a13db771c805392a26af5b948eb66ab6f1891c73
valen725 <juana.martinez@utp.edu.co>
2024-10-14 20:06:10 -0500

    test factura

--- Test/test_clase_factura.py
+++ Test/test_clase_factura.py
@@ -1,23 +1,49 @@
 import unittest
 from Modelo.clase_factura import Factura as factura
-from Modelo.clase_antibioticos import Antibiotico
-from Modelo.producto_fertilizante import ControlFertilizantes
-from Modelo.producto_plagas import ControlPlagas
+from Modelo.clase_antibioticos import Antibiotico as antibiotico
+from Modelo.producto_fertilizante import ControlFertilizantes as producto_fertilizante
+from Modelo.producto_plagas import ControlPlagas as producto_plaga
 
 
 class TestFactura(unittest.TestCase):
 
     def setUp(self):
-        """Inicializa los productos y una factura para las pruebas."""
-        self.pc_plagas = ControlPlagas("Plaga X", "ICA124", "8", "20000", 20)
-        self.pc_fertilizante = ControlFertilizantes("UREA", "ICA7028", "15", "60000", 15)
-        self.ant_1 = Antibiotico("anti_1", "10ml", "bovino", 120000, 10)
-        self.ant_2 = Antibiotico("anti_2", "8ml", "caprino", 100000, 8)
-        self.factura = factura()
+        self.pc_plaga = producto_plaga("Plaga X", "ICA124", "8", "20000")
+        self.pc_fertilizante = producto_fertilizante("UREA", "ICA7028", "15", "60000")
+        self.fact = factura.Factura()
 
-    
+    def test_vende_varios_producto_control(self):
+        self.fact.asociar_producto_control(self.pc_plaga)
+        self.fact.asociar_producto_control(self.pc_fertilizante)
 
-if __name__ == "__main__":
+        self.assertEqual(2,len(self.fact.producto_control), "No se ha asociado producto control")
+
+    def test_vende_varios_antibioticos(self):
+        ant_1 = antibiotico.Antibiotico("anti_1", "10ml", "bovino", "120000")
+        ant_2 = antibiotico.Antibiotico("anti_2", "8ml", "caprino", "100000")
+        self.fact.asociar_antibiotico(ant_1)
+        self.fact.asociar_antibiotico(ant_2)
+
+        self.assertEqual(2,len(self.fact.antibiotico), "No se ha asociado antibiotico")
+
+
+    def test_vende_varios_producto_control_antibioticos(self):
+        ant_1 = antibiotico.Antibiotico("anti_1", "10ml", "bovino", "120000")
+        ant_2 = antibiotico.Antibiotico("anti_2", "8ml", "caprino", "100000")
+        pc_plaga = producto_plaga.ControlPlagas("Plaga X", "ICA124", "8", "20000")
+        pc_fertilizante = producto_fertilizante.ControlFertilizantes("UREA", "ICA7028", "15", "60000")
+
+
+        self.fact.asociar_producto_control(pc_plaga)
+        self.fact.asociar_producto_control(pc_fertilizante)
+        self.fact.asociar_antibiotico(ant_1)
+        self.fact.asociar_antibiotico(ant_2)
+
+        self.assertEqual(2,len(self.fact.antibiotico), "No se ha asociado antibiotico")
+        self.assertEqual(2,len(self.fact.producto_control), "No se ha asociado antibiotico")
+
+
+if __name__ == "__main__":  
     unittest.main()
 
 
