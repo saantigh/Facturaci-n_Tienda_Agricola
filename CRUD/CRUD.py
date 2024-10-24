0bea74893bdad2f8fa1bdd2caeac829e4b0bc14d
valen725 <juana.martinez@utp.edu.co>
2024-10-24 10:26:24 -0500

    Test CRUD asquerosamente melo

--- CRUD/CRUD.py
+++ CRUD/CRUD.py
@@ -1,27 +1,24 @@
 class CRUD:
     def __init__(self):
-        self.clientes = []  # Lista de clientes
+        self.clientes = []
 
     def agregar_cliente(self, cliente):
         self.clientes.append(cliente)
 
     def buscar_por_cedula(self, cedula):
-        """Busca un cliente por su cédula."""
         for cliente in self.clientes:
             if cliente.cedula == cedula:
                 return cliente
         return None
 
     def eliminar_cliente(self, cedula):
-        """Elimina un cliente por cédula."""
         cliente = self.buscar_por_cedula(cedula)
-        if cliente:
-            self.clientes.remove(cliente)
-            print(f"Cliente con cédula {cedula} eliminado correctamente.")
-        else:
-            print(f"No se encontró ningún cliente con la cédula {cedula}.")
+        if cliente is None:
+            raise ValueError("No se puede eliminar, el cliente no existe.")
+        self.clientes.remove(cliente)
 
-    def asociar_factura(self, cliente, factura):
-        """Asocia una factura a un cliente ya existente"""
-        cliente.facturas = factura # Usa el método de cliente para agregar la factura
-        print(f"Factura asociada correctamente al cliente {cliente.nombre_cliente}")
+    def asociar_factura(self, cliente, facturas):
+        if cliente not in self.clientes:
+            raise ValueError("El cliente no existe.")
+        cliente.facturas.extend(facturas)
+
